9b9c6344e58d1b50eb42754be686eadcfca94c03
Brownies <100557588+NandaFarrel@users.noreply.github.com>
2023-03-08 17:17:49 +0700

    Create Poligon.java

---
+++ praktikum 3/org/poligon/Poligon.java
@@ -0,0 +1,13 @@
+// Nama: Nanda Farrel Chevalerie
+// Nim : 24060121140165
+// Deskripsi : representasi dasar dari objek poligon(segi banyak)
+
+package org.poligon;
+
+public class Poligon{
+	protected int jumlahSisi;
+	
+	public int getJumlahSisi(){
+		return this.jumlahSisi;
+	}
+}
