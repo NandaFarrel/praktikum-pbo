78c4237aa76c6553f9a4c17e5b3c2dbe689679ba
Brownies <100557588+NandaFarrel@users.noreply.github.com>
2023-03-08 15:33:30 +0700

    Update MOperasiTitik.java

--- praktikum 2/MOperasiTitik.java
+++ praktikum 2/MOperasiTitik.java
@@ -4,7 +4,7 @@
 
 class MOperasiTitik{
 	public static void main(String[] args){
-		Titik t1 = new Titik(5.0,3.0);
+		Titik t1 = new Titik(4.0,4.0);
 		OperasiTitik op = new OperasiTitik();
 		
 		System.out.println("Titik(" + t1.getAbsis() + "," + t1.getOrdinat() + ")");
