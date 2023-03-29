590d403b7ebb76cef19ae75477a57252b672de57
Brownies <100557588+NandaFarrel@users.noreply.github.com>
2023-03-29 09:56:21 +0700

    Create BujurSangkar.java

---
+++ praktikum 5/BujurSangkar.java
@@ -0,0 +1,10 @@
+// Nama: Nanda Farrel Chevalerie
+// Nim : 24060121140165
+ * Deskripsi : Kelas yang membuat implementasi metode abstrak pada bangun datar
+
+public class BujurSangkar extends BangunDatar{
+    public double hitungLuas(double sisi){
+        double luas = sisi * sisi;
+        return luas;
+    }
+}
