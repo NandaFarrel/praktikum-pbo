aba8368df7e6577de9c2fa984e36f040163b936c
Brownies <100557588+NandaFarrel@users.noreply.github.com>
2023-03-29 09:57:14 +0700

    Create IArea.java

---
+++ praktikum 5/IArea.java
@@ -0,0 +1,7 @@
+// Nama: Nanda Farrel Chevalerie
+// Nim : 24060121140165
+//Deskripsi : Interface yang mendefinisikan kontrak bahwa setiap kelas yang membuat implementasi interface ini harus memiliki cara untuk menghitung luas
+
+public interface IArea {
+    public double hitungLuas();
+}
