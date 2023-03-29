f5bb28c82ab8990c7ebcde84a42ee17ed1930855
Brownies <100557588+NandaFarrel@users.noreply.github.com>
2023-03-29 09:58:02 +0700

    Create Lingkaran.java

---
+++ praktikum 5/Lingkaran.java
@@ -0,0 +1,17 @@
+// Nama: Nanda Farrel Chevalerie
+// Nim : 24060121140165
+// Deskripsi : Kelas implementasi IArea berupa cara menghitung luas lingkaran
+
+import static java.lang.Math.PI;
+
+class Lingkaran implements IArea {
+    private double jejari;
+
+    public Lingkaran(double r) {
+        jejari = r;
+    }
+
+    public double hitungLuas() {
+        return PI * jejari * jejari;
+    }
+}
