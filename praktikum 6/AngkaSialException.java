2410ac664119b1a6de830cb960d90fb696d87780
Brownies <100557588+NandaFarrel@users.noreply.github.com>
2023-04-05 19:09:54 +0700

    Create AngkaSialException.java

---
+++ praktikum 6/AngkaSialException.java
@@ -0,0 +1,9 @@
+// Nama: Nanda Farrel Chevalerie
+// Nim : 24060121140165
+// Deskripsi	: Eksepsi buatan sendiri, menolak masukkan angka 13!
+
+public class AngkaSialException extends Exception{
+	public AngkaSialException(){
+		super("jangan memasukkan angka 13 karena angka sial !!!");
+	}
+}
