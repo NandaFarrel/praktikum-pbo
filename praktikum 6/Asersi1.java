0525b409d7e18c7f11732e96d548e191514b8915
Brownies <100557588+NandaFarrel@users.noreply.github.com>
2023-04-05 19:10:23 +0700

    Create Asersi1.java

---
+++ praktikum 6/Asersi1.java
@@ -0,0 +1,16 @@
+// Nama: Nanda Farrel Chevalerie
+// Nim : 24060121140165
+// Deskripsi	: Program untuk menunjukkan asersi
+
+public class Asersi1{
+	public static void main(String[] args){
+		int x=0;
+		if(x>0){
+			System.out.println("x bilangan positif");
+		}
+		else{
+			assert(x<0):"ada kesalahan kode";
+			System.out.println("x bilangan negatif");
+		}
+	}
+}
