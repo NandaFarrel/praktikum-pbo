26197788ff45a384d5a0c683c44ac607d65b99b4
Brownies <100557588+NandaFarrel@users.noreply.github.com>
2023-05-10 05:44:20 +0700

    Create Payroll.java

---
+++ praktikum 7/Payroll.java
@@ -0,0 +1,9 @@
+// Nama: Nanda Farrel Chevalerie
+// Nim : 24060121140165
+// Deskripsi	: File pencetak gaji dari Pegawai
+
+public class Payroll{
+	public void cetakGaji(Pegawai p){
+		p.tampilData();
+	}
+}
