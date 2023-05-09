2c2f43ce27b7865c73f94bfcc4537baebe83e990
Brownies <100557588+NandaFarrel@users.noreply.github.com>
2023-05-10 05:43:42 +0700

    Update Manager.java

--- praktikum 7/Manager.java
+++ praktikum 7/Manager.java
@@ -3,15 +3,12 @@
 // Deskripsi	: File subclass dari Pegawai
 
 public class Manajer extends Pegawai{
-	// Attribute Manajer (overiding Pegawai)
 	protected int tunjangan = 700000;
 	
-	// Method mengisi nama Manajer
 	public Manajer(String name){
 		nama = name;
 	}
 	
-	// Method menampilkan nama, gaji pokok Pegawai, dan tunjangan Manajer
 	public void tampilData(){
 		System.out.println("Nama = "+nama+", Gaji pokok : "+gajiPokok+", Tunjangan : "+tunjangan);
 	}
