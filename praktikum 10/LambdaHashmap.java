edfacc75015a2bfc3e23c001ffd9055e92fe44c2
Brownies <100557588+NandaFarrel@users.noreply.github.com>
2023-06-06 20:50:50 +0700

    Update LambdaHashmap.java

--- praktikum 10/LambdaHashmap.java
+++ praktikum 10/LambdaHashmap.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class LambdaHashmap{ 
     public static void main(String[] args){ 
         Map<String, String> mahasiswaMap = new HashMap<>(); 
-        mahasiswaMap.put("24060121120023", "Arynda Anna Salsabiela"); 
+        mahasiswaMap.put("24060121140165", "Nanda Farrel Chevalerie"); 
  
         mahasiswaMap.forEach((nim, nama) -> System.out.println("NIM: " +nim+ ", Nama: " +nama)); 
     } 
