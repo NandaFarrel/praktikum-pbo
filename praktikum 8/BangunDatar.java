73a4498d4c81e16324678027217fb81ebdab5886
Brownies <100557588+NandaFarrel@users.noreply.github.com>
2023-06-06 18:27:39 +0700

    Create BangunDatar.java

---
+++ praktikum 8/BangunDatar.java
@@ -0,0 +1,7 @@
+// Nama: Nanda Farrel Chevalerie
+// Nim : 24060121140165
+// Deskripsi : kelas abstrak untuk bangun datar
+
+public abstract class BangunDatar {
+    public abstract double hitungKeliling();
+}
