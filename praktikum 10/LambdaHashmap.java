e76f6c11fef8bf5254dcef1fcb04f1a7b652fe0f
Brownies <100557588+NandaFarrel@users.noreply.github.com>
2023-06-06 18:59:27 +0700

    Create LambdaHashmap.java

---
+++ praktikum 10/LambdaHashmap.java
@@ -0,0 +1,15 @@
+// Nama: Nanda Farrel Chevalerie
+// Nim : 24060121140165
+// Deskripsi : Implementasi lambda pada Hashmap
+
+import java.util.HashMap; 
+import java.util.Map; 
+ 
+public class LambdaHashmap{ 
+    public static void main(String[] args){ 
+        Map<String, String> mahasiswaMap = new HashMap<>(); 
+        mahasiswaMap.put("24060121120023", "Arynda Anna Salsabiela"); 
+ 
+        mahasiswaMap.forEach((nim, nama) -> System.out.println("NIM: " +nim+ ", Nama: " +nama)); 
+    } 
+}
